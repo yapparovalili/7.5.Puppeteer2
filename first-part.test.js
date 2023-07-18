1e89c75fe8d6e70dd30ea57a6f25a541e6a16b51
Liliya Yapparova <yapparovalili@yandex.ru>
2023-07-19 02:20:56 +0700

    1

---
+++ first-part.test.js
@@ -0,0 +1,51 @@
+const puppeteer = require("puppeteer");
+const {clickElement, getText, putText} = require("./lib/commands.js");
+
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+  await page.goto("http://qamid.tmweb.ru/client/index.php");
+});
+
+afterEach(() => {
+  page.close();
+});
+
+describe("Tests for booking tickets", () => {
+    test("Should book available ticket", async () => {
+      await clickElement(page, "body nav a:nth-child(2)");
+      await clickElement(page, "body main section:nth-child(2) div:nth-child(3) ul li a");
+      await clickElement(page, ".buying-scheme__wrapper div:nth-child(7) span:nth-child(2)");
+      await clickElement(page, "button.acceptin-button");
+      await page.waitForSelector("h2.ticket__check-title");
+      await clickElement(page, "button.acceptin-button");
+      const actual = await getText(page, "h2.ticket__check-title");
+      expect(actual).toContain("Электронный билет");
+    });
+  
+    test("Should book some available tickets", async () => {
+      await clickElement(page, "body nav a:nth-child(2)");
+      await clickElement(page, "body main section:nth-child(2) div:nth-child(3) ul li a");
+      await clickElement(page, ".buying-scheme__wrapper div:nth-child(9) span:nth-child(2)");
+      await clickElement(page, ".buying-scheme__wrapper div:nth-child(9) span:nth-child(6)");
+      await clickElement(page, "button.acceptin-button");
+      await page.waitForSelector("h2.ticket__check-title");
+      await clickElement(page, "button.acceptin-button");
+      const actual = await getText(page, "h2.ticket__check-title");
+      expect(actual).toContain("Электронный билет");
+    });
+  
+    test("Should try to book unavailable ticket, but unsuccessfully", async () => {
+      await clickElement(page, "body nav a:nth-child(2)");
+      await clickElement(page, "body main section:nth-child(2) div:nth-child(3) ul li a");
+      await clickElement(page, ".buying-scheme__wrapper div:nth-child(9) span:nth-child(2)");
+      expect(
+        String(
+          await page.$eval("button", (button) => {
+            return button.disabled;
+          })
+        )
+      ).toContain("true");
+    });
+  });
